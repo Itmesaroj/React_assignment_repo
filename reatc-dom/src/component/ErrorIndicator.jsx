01d11e2ecefc998dd93e70a06e7ad27fb4d3f388
itmesaroj <itsmesaroj007@gmail.com>
2024-04-23 05:18:26 +0530

    react-dom-1

---
+++ reatc-dom/src/component/ErrorIndicator.jsx
@@ -0,0 +1,9 @@
+// src/components/ErrorIndicator.jsx
+import React from "react";
+
+function ErrorIndicator() {
+  return <div>Error occurred. Please try again later.</div>;
+}
+
+export default ErrorIndicator;
+

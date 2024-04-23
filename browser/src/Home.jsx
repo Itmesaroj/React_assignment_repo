92d3776799a8eb7f6d9a9dd1a03a3de71a5d7226
itmesaroj <itsmesaroj007@gmail.com>
2024-04-23 12:23:26 +0530

    Browser

---
+++ browser/src/Home.jsx
@@ -0,0 +1,15 @@
+// Home.js
+
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to MyApp!</h2>
+      <p>MyApp is a platform where users can interact with each other and share content.</p>
+      {/* Add more relevant content */}
+    </div>
+  );
+}
+
+export default Home;
